4d717d108edaef034862f8cf25a3e8808e1f1038
cygus84 <cygus84@gmail.com>
2024-07-18 12:56:52 +0100

    ac
    ac

---
+++ Shronisko-001/src/app/StartGry.java
@@ -0,0 +1,10 @@
+package app;
+
+public class StartGry {
+
+	public static void main(String[] args) {
+		System.out.println("Shronisko-001");
+
+	}
+
+}
---
+++ Shronisko-001/src/app/modele/Duszek.java
@@ -0,0 +1,8 @@
+package app.modele;
+
+public class Duszek {
+
+	private String nazwa;
+	private int wiek;
+	private boolean dorosly;
+}
